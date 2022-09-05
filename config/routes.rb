9574cba3cc9550575272094147d40d89f2264554
rahulgupta34 <rahul.g@simformsolutions.com>
2022-09-05 16:25:45 +0530

    Rails Tubro_stream practice crud [05/09/2022] create user scaffold amd add some validation on it’s attribute add menu bar on it to navigate the application modify users_controller action for turbo_stream modify edit for get request to post request in routes also modify user and index template for it’s content add counting feature to user crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users do
+    member do
+      post :edit
+    end
+  end
+  
+  root "users#index"
 end
